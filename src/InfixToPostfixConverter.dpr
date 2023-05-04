1e3c5980934a966420fedb648f8f4b8613e33368
Egor Pankratiev <egor.pankratiew@gmail.com>
2023-05-04 13:44:35 +0300

    Upgrade programm structure

--- src/InfixToPostfixConverter.dpr
+++ src/InfixToPostfixConverter.dpr
@@ -6,132 +6,141 @@ uses
   System.SysUtils,
   System.Generics.Collections;
 
+type
+  TExpressionWithRank = record
+    ExpressionRank: integer;
+    Expression: string;
+  end;
+
 var
-  ExpressionRank: integer;
-  Expression: string;
+  ExpressionWithRank: TExpressionWithRank;
+  InputExpression: string;
 
-Procedure InputExpression(var AExpression: string);
-begin
-  write('Input an expression to convert it from infix form to postfix form (reversed polish notation) and count its rank: ');
-  readln(AExpression);
-end;
-
-Function GetPrecedence(ASymbol: char): integer;
+function GetPrecedence(ASymbol: char): Integer;
 begin
   case ASymbol of
-    'A'..'Z', 'a'..'z', '0'..'9': Result := 7;
-    '+', '-': Result := 1;
-    '*', '/': Result := 3;
-    '^': Result := 6;
-    '(': Result := 9;
-    ')': Result := 0;
-  else
-    Result := -1;
+    'A'..'Z', 'a'..'z', '0'..'9':
+      Result := 7;
+    '+', '-':
+      Result := 1;
+    '*', '/':
+      Result := 3;
+    '^':
+      Result := 6;
+    '(':
+      Result := 9;
+    ')':
+      Result := 0;
+    else
+      Result := -1;
   end;
 end;
 
-Function GetStackPrecedence(ASymbol: char): integer;
+function GetStackPrecedence(ASymbol: char): Integer;
 begin
   case ASymbol of
-    'A'..'Z', 'a'..'z', '0'..'9': Result := 8;
-    '+', '-': Result := 2;
-    '*', '/': Result := 4;
-    '^': Result := 5;
-    '(': Result := 0;
-  else
-    Result := -1;
-  end;
-
-end;
-
-Function GetSymbolRank(ASymbol: char): integer;
-begin
-  case ASymbol of
-    '+', '-', '*', '/', '^': Result := -1;
-    'A'..'Z', 'a'..'z': Result := 1;
-  else
-    Result := 0;
+    'A'..'Z', 'a'..'z', '0'..'9':
+      Result := 8;
+    '+', '-':
+      Result := 2;
+    '*', '/':
+      Result := 4;
+    '^':
+      Result := 5;
+    '(':
+      Result := 0;
+    else
+      Result := -1;
   end;
 end;
 
-Function InfixToPostfix(var AExpressionRank: integer; AExpression: string): string;
+function GetSymbolRank(ASymbol: char): Integer;
+begin
+  case ASymbol of
+    '+', '-', '*', '/', '^':
+      Result := -1;
+    'A'..'Z', 'a'..'z', '0'..'9':
+      Result := 1;
+    else
+      Result := 0;
+  end;
+end;
+
+function ConverInfixToPostfix(const AExpression: string): TExpressionWithRank;
 var
   Stack: TStack<Char>;
-  Queue: string;
-  i: integer;
+  I, Precedence: Integer;
 begin
-  Queue:= '';
+  Result.Expression:= '';
   Stack:= TStack<Char>.Create;
-  AExpressionRank:= 0;
+  Result.ExpressionRank:= 0;
 
-  for I := 1 to length(AExpression) do
+  for I := 1 to High(AExpression) do
   begin
+    Precedence:= GetPrecedence(AExpression[i]);
 
-    // If the character is an operand, add it to output.
-    if GetPrecedence(AExpression[i]) = 7 then
+    // If the scanned character is an open bracket, push it to the stack
+    if Precedence = 9 then
+      Stack.Push(AExpression[i])
+
+    // If the character is an operand, add it to output
+    else if Precedence = 7 then
     begin
-  		Queue := Queue + AExpression[i];
-      AExpressionRank := AExpressionRank + GetSymbolRank(AExpression[i]);
+  		Result.Expression := Result.Expression + AExpression[i];
+      Inc(Result.ExpressionRank, GetSymbolRank(AExpression[i]));
     end
-    else
 
-      // If the scanned character is an open bracket, push it to the stack.
-      if GetPrecedence(AExpression[i]) = 9 then
-        Stack.Push(AExpression[i])
-    else
-
-      // If the scanned character is an
-      // close bracket, pop and add to output from the stack
-      // until an open bracket is encountered.
-      if GetPrecedence(AExpression[i]) = 0 then
+    // If the scanned character is an
+    // close bracket, pop and add to output from the stack
+    // until an open bracket is encountered
+    else if Precedence = 0 then
+    begin
+      while (Stack.Count <> 0) and (GetStackPrecedence(Stack.Peek) <> 0) do
       begin
-        while (Stack.Count <> 0) and (GetStackPrecedence(Stack.Peek) <> 0) do
-        begin
-          Queue := Queue + Stack.Peek;
-          AExpressionRank := AExpressionRank + GetSymbolRank(Stack.Peek);
-          Stack.Pop;
-        end;
-
-        // Remove open bracket from the stack
+        Result.Expression := Result.Expression + Stack.Peek;
+        Result.ExpressionRank := Result.ExpressionRank + GetSymbolRank(Stack.Peek);
         Stack.Pop;
-      end
-    else
-      begin
-        while ((Stack.Count <> 0) and (GetPrecedence(AExpression[i]) <= GetStackPrecedence(Stack.Peek))) do
-        begin
-          AExpressionRank := AExpressionRank + GetSymbolRank(Stack.Peek);
-          Queue := Queue + Stack.Peek;
-          Stack.Pop;
-        end;
-      Stack.Push(AExpression[i]);
       end;
 
+      // Remove open bracket from the stack
+      Stack.Pop;
+    end
+
+    else
+    begin
+      while ((Stack.Count <> 0) and (GetPrecedence(AExpression[i]) <= GetStackPrecedence(Stack.Peek))) do
+      begin
+        Result.ExpressionRank := Result.ExpressionRank + GetSymbolRank(Stack.Peek);
+        Result.Expression := Result.Expression + Stack.Peek;
+        Stack.Pop;
+      end;
+      Stack.Push(AExpression[i]);
+    end;
   end;
 
   while not (Stack.Count = 0) do
   begin
-    AExpressionRank := AExpressionRank + GetSymbolRank(Stack.Peek);
-    Queue := Queue + Stack.Peek;
+    Result.ExpressionRank := Result.ExpressionRank + GetSymbolRank(Stack.Peek);
+    Result.Expression := Result.Expression + Stack.Peek;
     Stack.Pop;
   end;
 
   Stack.Destroy;
-  Result := Queue;
 end;
 
-Procedure Output(AAExpression: string; var AAExpressionRank: integer);
+
 begin
-  writeln('Postfix form: ' ,InfixToPostfix(AAExpressionRank, AAExpression));
-  writeln('Expression rank: ', AAExpressionRank);
-  if AAExpressionRank = 1 then
-    writeln('Expression is accurate.')
+  Writeln('Input an expression to convert it from infix form to postfix form (reversed polish notation) and count its rank:');
+  Readln(InputExpression);
+
+  ExpressionWithRank:= ConverInfixToPostfix(InputExpression);
+
+  Writeln('Postfix form: ', ExpressionWithRank.Expression);
+  Writeln('Expression rank: ', ExpressionWithRank.ExpressionRank);
+  if ExpressionWithRank.ExpressionRank = 1 then
+    Writeln('Expression is accurate')
   else
-    writeln('Expression is inaccurate.')
-end;
+    Writeln('Expression is inaccurate');
 
-
-begin
-  InputExpression(Expression);
-  Output(Expression, ExpressionRank);
-  readln;
+  Readln;
 end.
